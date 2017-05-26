4e94306b91ab8b1bc0e3899a4ffabc19ae36669e
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-05-26 17:49:42 +0200

    switched to leaflet

--- main.js
+++ main.js
@@ -1,64 +1,128 @@
-/*
- * Copyright (C) 2014 United States Government as represented by the Administrator of the
- * National Aeronautics and Space Administration. All Rights Reserved.
- */
-var wwd;
-require(['./src/WorldWind', './LayerManager'], function (ww, LayerManager) {
-    "use strict";
-    function addOSM(layerManager, geojson) {
-        var request = new XMLHttpRequest();
-        request.open("GET", "http://ows.terrestris.de/osm/service?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetCapabilities", true);
-        request.onreadystatechange = function () {
-            if (request.readyState === 4 && request.status === 200) {
-                var xmlDom = request.responseXML;
-
-                if (!xmlDom && request.responseText.indexOf("<?xml") === 0) {
-                    xmlDom = new window.DOMParser().parseFromString(request.responseText, "text/xml");
-                }
-                var wmsCapsDoc = new WorldWind.WmsCapabilities(xmlDom);
-                var config = WorldWind.WmsLayer.formLayerConfiguration(wmsCapsDoc, null);
-                config.title = "OpenStreetMap";
-                config.layerNames = "OSM-WMS";
-                var layer = new WorldWind.WmsLayer(config, null);
-                layer.detailControl = 1.5;
-                wwd.addLayer(layer);
-            }
-        }
-    };
-    WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
-
-    wwd = new WorldWind.WorldWindow("canvasOne");
-
-    var layers = [
-        {layer: new WorldWind.BMNGLayer(), enabled: true},
-        {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
-        {layer: new WorldWind.BingAerialLayer(null), enabled: false},
-        {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: false},
-        {layer: new WorldWind.BingRoadsLayer(null), enabled: false},
-        {layer: new WorldWind.CompassLayer(), enabled: true},
-        {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
-        {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
-    ];
-
-    for (var l = 0; l < layers.length; l++) {
-        layers[l].layer.enabled = layers[l].enabled;
-        wwd.addLayer(layers[l].layer);
-    }
-
-    var modelLayer = new WorldWind.RenderableLayer("model");
-    wwd.addLayer(modelLayer);
-
-    var position = new WorldWind.Position(45, -100, 0);
-    var colladaLoader = new WorldWind.ColladaLoader(position);
-    colladaLoader.init({dirPath: './collada_models/'});
-    colladaLoader.load('building.dae', function (scene) {
-        scene.scale = 50;
-        modelLayer.addRenderable(scene);
+var osmUrl = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    osm = new L.TileLayer(osmUrl, {
+        maxZoom: 22,
+        attribution: "Map data &copy; OpenStreetMap contributors"
     });
 
-    addOSM();
-
-
-    new LayerManager(wwd);
-
+var map = new L.Map('map', {
+    layers: [osm],
+    center: new L.LatLng(45.499688842786, 9.230298207042001),
+    zoom: 19
 });
+
+
+var indoorLayer = new L.Indoor(GeoJSON, {
+
+    onEachFeature: function (feature, layer) {
+        if (feature.properties.view) {
+            var container = $('<Button class="startPanoButton" />');
+            container.html("Panorama");
+            container.on('click', function () {
+                startPano(feature.properties.view)
+            });
+            layer.bindPopup(container[0]);
+        } else {
+            layer.bindPopup(JSON.stringify(feature.properties.id, null, 4));
+        }
+
+    },
+    style: function (feature) {
+        var fill = 'white';
+
+        if (feature.properties.id === 'room') {
+            fill = '#169EC6';
+        } else if (feature.properties.id === 'room') {
+            fill = '#0A485B';
+        }
+
+        return {
+            fillColor: fill,
+            weight: 1,
+            color: '#666',
+            fillOpacity: 1
+        };
+    }
+});
+
+indoorLayer.setLevel("1");
+
+
+var levelControl = new L.Control.Level({
+    level: "1",
+    levels: indoorLayer.getLevels()
+});
+
+
+function get3d(myJson, level) {
+    osmb.each(function (p) {
+        if (p.properties.level !== level) {
+            return false
+        }
+    });
+    osmb.set(myJson);
+}
+function customListener(num) {
+    indoorLayer.setLevel(num);
+    get3d(GeoJSON, num.newLevel);
+}
+
+
+levelControl.addEventListener("levelchange", customListener, indoorLayer);
+levelControl.addTo(map);
+indoorLayer.addTo(map);
+
+
+var osmb = new OSMBuildings(map);
+get3d(GeoJSON, 1);
+
+
+var viewer;
+
+
+var imagesList = ["equirectangular.jpg", "room1.jpg"];
+var panoList = [];
+var initialized = 0;
+
+function initPanorama() {
+    imagesList.forEach(function (p, i) {
+        panoList.push(new PANOLENS.ImagePanorama("image/" + imagesList[i]));
+    });
+    var view3d = document.getElementById("view3d");
+    viewer = new PANOLENS.Viewer({
+        container: view3d,	// A DOM Element container
+        controlBar: true,
+        autoHideControlBar: false,	// Auto hide control bar
+        autoHideInfospot: true,		// Auto hide infospots
+        horizontalView: false,		// Allow only horizontal camera control
+        cameraFov: 120,				// Camera field of view in degree
+        reverseDragging: false,		// Reverse orbit control direction
+        enableReticle: false,		// Enable reticle for mouseless interaction
+        dwellTime: 1500,			// Dwell time for reticle selection in millisecond
+        autoReticleSelect: true,	// Auto select a clickable target after dwellTime
+        passiveRendering: false	// Render only when control triggered by user input
+    });
+    panoList.forEach(function(p){
+        viewer.add(p);
+    })
+
+
+}
+function startPano(num) {
+    $("#view3d").show();
+    $("#map").hide();
+
+    if (!initialized) {
+        initPanorama();
+        initialized=1;
+    }
+    viewer.setPanorama(panoList[num-1]);
+
+    $(".startMapButton").show();
+}
+
+function startMap() {
+    $("#view3d").hide();
+    $("#map").show();
+    $("#startPanoButton").show();
+    $("#startMapButton").hide();
+}
